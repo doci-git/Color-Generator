9661d06bcd3058280458444e6aabfc95183513bd
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-03 06:51:29 +0200

    hex logic

---
+++ app.js
@@ -0,0 +1,10 @@
+const hex = ['A', 'B', 'C', 'D', 'E', 'F', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const btn = document.querySelector('button');
+const tittle = document.querySelector('h1');
+
+btn.addEventListener('click', () => {
+    let hexcolor = '#';
+    for (let i=0; i < 6; i++) {
+
+    }
+})
