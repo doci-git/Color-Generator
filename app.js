ef77d20148431fa725d6375f67c7d9808e50a08b
Musa Doci <81824102+doci-git@users.noreply.github.com>
2022-10-05 06:43:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const tittle = document.querySelector('h1');
 
 btn.addEventListener('click', () => {
     let hexcolor = '#';
-    for (let i=0; i < 6; i++) {
-
+    for (let i = 0; i < 6; i++) {
+        hexcolor += hex[random()]
     }
-})
+    document.body.style.backgroundColor = hexcolor
+    const d = document.querySelector('span').innerHTML = hexcolor
+});
+
+function random() {
+    return Math.floor(Math.random() * hex.length);
+
+}
